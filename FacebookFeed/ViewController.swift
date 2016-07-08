c984dacbe6b55eaeeba6753dcf62685496af547b
tyrionchiang <s22194869@gmail.com>
2016-07-08 09:04:56 +0800

    Initial Commit

---
+++ FacebookFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookFeed
+//
+//  Created by Chiang Chuan on 7/8/16.
+//  Copyright © 2016 Chiang Chuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
